7435f42d2dcf36c7e4e3f5e42b15fe56bdbf576e
Shauryan  K <shauryan.kanaujia@gmail.com>
2024-06-22 12:20:20 -0700

    packets test

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from audio_recorder_streamlit import audio_recorder
 from openai import OpenAI
+import tempfile
 import os
 import uuid
 from elevenlabs.client import ElevenLabs
@@ -79,14 +80,11 @@ audio_bytes = audio_recorder(
     icon_size="1x",
 )
 
-path = os.path.join('responses', f"{uuid.uuid4()}.mp3")
-with open(path, "wb") as f:
-    f.write(audio_bytes)
-    
-
-#audio display
-#if audio_bytes:
-#st.audio(audio_bytes, format="audio/mp3")
+if audio_bytes:
+    packet = tempfile.NamedTemporaryFile()
+    packet.write(audio_bytes)
+    if packet:
+        st.audio(packet.name, format="audio/mp3")
 
 if st.button("Send", key="send"):
     user_input = st.session_state.get("input", "")
